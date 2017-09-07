5bad3f45bdca436515b416bbcfb45ca53b46ca2a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-09-07 16:28:36 -0300

    Add get_data method to data importer
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- application/lomadee/data_importer.py
+++ application/lomadee/data_importer.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from urllib.parse import urljoin, urlencode
+import requests
 
 
 class ComputerDataImporter(object):
@@ -11,7 +12,29 @@ class ComputerDataImporter(object):
         api_url = urljoin(settings.LOMADEE_API_URL, settings.LOMADEE_APP_TOKEN)
 
         # Specific path to 'Computer' category
-        url = urljoin(api_url, 'offer/_category/6424')
+        url = urljoin('{}/'.format(api_url), 'offer/_category/6424')
         kwargs['sourceId'] = settings.LOMADEE_SOURCE_ID
+        kwargs['size'] = 100
 
         return '{}?{}'.format(url, urlencode(kwargs))
+
+    def get_data(self, url=None):
+        if not url:
+            url = self.build_api_url()
+
+        data = requests.get(url).json()
+        if data['requestInfo']['status'] != 'OK':
+            return False
+
+        final_data = []
+        final_data.extend(data['offers'])
+        pagination = data['pagination']
+
+        # Get only 3 pages. To get all pages use:
+        # if pagination['page'] < pagination['totalPage']
+        if pagination['page'] < 3:
+            next_page_data = self.get_data(
+                self.build_api_url(page=pagination['page'] + 1)
+            )
+            final_data.extend(next_page_data)
+        return final_data
