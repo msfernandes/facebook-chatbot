bd840e4afc4e80834c68b33702bf45a1167f7483
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-09-08 17:24:58 -0300

    Add specs extraction methods

--- application/lomadee/data_importer.py
+++ application/lomadee/data_importer.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from urllib.parse import urljoin, urlencode
+import re
 import requests
 import requests_cache
 
@@ -40,3 +41,55 @@ class ComputerDataImporter(object):
             )
             final_data.extend(next_page_data)
         return final_data
+
+    def get_specs(self, description):
+        cpu = self.get_cpu(description)
+        ram = self.get_ram(description)
+        disk = self.get_disk(description)
+        is_macbook = self.is_macbook(description)
+        has_gpu = self.has_gpu(description)
+        has_ssd = self.has_ssd(description)
+        return (cpu, ram, disk, is_macbook, has_gpu, has_ssd)
+
+    def get_cpu(self, description):
+        groups = self._get('(i3|i5|i7)', description)
+        if groups:
+            return groups[0]
+
+    def get_disk(self, description):
+        groups = self._get('\s(\d{1})\s?(tb|tera)|\s(\d{3,4})\s?(hd|gb)',
+                           description)
+        if groups:
+            tb, _, gb, _ = groups
+            if tb:
+                return int(tb) * 1000
+            else:
+                return int(gb)
+
+    def get_ram(self, description):
+        groups = self._get('\s(\d{1,2})\s?(gb|ram|ddr)', description)
+        if groups:
+            return int(groups[0])
+
+    def is_macbook(self, description):
+        return self._has('apple|macbook', description)
+
+    def has_gpu(self, description):
+        return self._has('geforce|gtx|dedicad[ao]|nvidea|radeon', description)
+
+    def has_ssd(self, description):
+        return self._has('ssd|solid state', description)
+
+    def _has(self, regex, description):
+        match = re.search(regex, description.lower())
+        if match:
+            return True
+        else:
+            return False
+
+    def _get(self, regex, description):
+        match = re.search(regex, description.lower())
+        if match:
+            return match.groups()
+        else:
+            return None
