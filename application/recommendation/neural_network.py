03db692bebe12ac4820e4558686b275b56656ecc
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-09-15 01:40:34 -0300

    update neural network
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- application/recommendation/neural_network.py
+++ application/recommendation/neural_network.py
@@ -4,8 +4,29 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from lomadee import models
-from recommendation import queries
+from django.db.models import Q
+import numpy as np
 import os
+
+
+QUERIES = {
+    'cpu': {0: Q(cpu='i3'),
+            1: Q(cpu='i5'),
+            2: Q(cpu='i7')},
+    'ram': {0: Q(ram__lte=4),
+            1: Q(ram__gt=4, ram__lte=12),
+            2: Q(ram__gt=12)},
+    'disk': {0: Q(),
+             1: Q(disk__gte=1000)},
+}
+
+
+def get_query(name, array):
+    chosen = np.where(array)[0]
+    if len(chosen):
+        return QUERIES[name][chosen[0]]
+    else:
+        return QUERIES[name][0]
 
 
 class Classifier():
@@ -31,7 +52,7 @@ class Classifier():
 
     def _classifier(self, x_train, y_train):
         classifier = MLPClassifier(hidden_layer_sizes=(11, 14, 18),
-                                   max_iter=10000)
+                                   max_iter=5000)
         classifier.fit(self.trainset_input(), self.trainset_output())
         return classifier
 
@@ -45,9 +66,9 @@ class Classifier():
     def suggest(self, **kwargs):
         suggestion_profile = self.classify(**kwargs)[0]
         computers = models.Computer.objects.filter(
-            queries.get_query('cpu', suggestion_profile[:3]),
-            queries.get_query('ram', suggestion_profile[3:6]),
-            queries.QUERIES['disk'][suggestion_profile[8:9].item()],
+            get_query('cpu', suggestion_profile[:3]),
+            get_query('ram', suggestion_profile[3:6]),
+            QUERIES['disk'][suggestion_profile[8:9].item()],
             is_macbook=bool(suggestion_profile[9]),
             has_ssd=bool(suggestion_profile[6]),
             has_gpu=bool(suggestion_profile[7]),
